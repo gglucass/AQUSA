a8c87fadf4a3c98b2a80115ba559105c3aef9946
Garm Lucassen <garm.lucassen@gmail.com>
2015-09-16 22:27:48 +0200

    Don't break with subscriptable on empty form

--- app/views.py
+++ app/views.py
@@ -62,10 +62,10 @@ def project(project_unique):
 def update_error(project_unique, error_id):
   project = Project.query.get(project_unique)
   error = project.errors.filter_by(id=error_id).first()
-  if request.form['false_positive'] == 'True':
+  if request.form.get('false_positive', None) == 'True':
     error.false_positive = True
     error.save()
-  if request.form.get['correct_minor_issue'] == 'True':
+  if request.form.get('correct_minor_issue', None) == 'True':
     error.correct_minor_issue()
     error.story.re_chunk()
     error.story.re_analyze()
