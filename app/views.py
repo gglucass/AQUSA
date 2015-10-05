a176c15b5e382aede08a53df338d326367e68a7d
Garm Lucassen <garm.lucassen@gmail.com>
2015-10-05 15:37:19 +0200

    Rename error to defect

--- app/views.py
+++ app/views.py
@@ -2,12 +2,13 @@ from flask import jsonify, abort, session, render_template, request, flash, redi
 from werkzeug import secure_filename
 import os
 from app import app, babel
-from .models import Story, Project, Error
+from .models import Stories, Projects, Defects
 from config import LANGUAGES
+import json
 
 @app.route('/')
 def index():
-  projects = Project.query.all()
+  projects = Projects.query.all()
   return render_template('index.html', title='Home', projects=projects)
 
 def allowed_file(filename):
@@ -16,7 +17,7 @@ def allowed_file(filename):
 
 @app.route('/project/<string:project_unique>/upload_file', methods=['GET', 'POST'])
 def upload_file(project_unique):
-  project = Project.query.get(project_unique)
+  project = Projects.query.get(project_unique)
   if request.method == 'POST':
     file = request.files['file']
     print(file.filename)
@@ -32,68 +33,82 @@ def upload_file(project_unique):
 @app.route('/projects/new', methods=['GET', 'POST'])
 def new_project():
   if request.method == 'POST':
-    project = Project.create(request.form['name'])
+    project = Projects.create(request.form['name'])
     return redirect(url_for('project', project_unique=project.id))
   return render_template('new_project.html', title='New Project')
 
 
 @app.route('/report', methods=['GET'])
-def error_report():
-  stories = Story.query.order_by('id').all()
-  return render_template('report.html', title='Error report', stories=stories)
+def defect_report():
+  stories = stories.query.order_by('id').all()
+  return render_template('report.html', title='Defect report', stories=stories)
 
 @app.route('/project/<string:project_unique>', methods=['GET'])
 def project(project_unique):
-  project = Project.query.get(project_unique)
-  project_errors = project.errors.filter_by(false_positive=False).all()
-  severe_errors = project.errors.filter_by(severity="high", false_positive=False).all()
-  medium_errors = project.errors.filter_by(severity="medium", false_positive=False).all()
-  minor_errors = project.errors.filter_by(severity="minor", false_positive=False).all()
-  false_positives = project.errors.filter_by(false_positive=True).all()
-  perfect_stories = project.stories.filter(Story.errors == None).all()
+  project = Projects.query.get(project_unique)
+  project_defects = project.defects.filter_by(false_positive=False).all()
+  severe_defects = project.defects.filter_by(severity="high", false_positive=False).all()
+  medium_defects = project.defects.filter_by(severity="medium", false_positive=False).all()
+  minor_defects = project.defects.filter_by(severity="minor", false_positive=False).all()
+  false_positives = project.defects.filter_by(false_positive=True).all()
+  perfect_stories = project.stories.filter(Stories.defects == None).all()
   stories = project.stories.order_by('id').all()
 
   return render_template('report.html', title=project.name, project=project, 
-    stories=stories, severe_errors=severe_errors, medium_errors=medium_errors, minor_errors=minor_errors, 
-    false_positives=false_positives, perfect_stories=perfect_stories, project_errors=project_errors)
+    stories=stories, severe_defects=severe_defects, medium_defects=medium_defects, minor_defects=minor_defects, 
+    false_positives=false_positives, perfect_stories=perfect_stories, project_defects=project_defects)
 
-@app.route('/project/<string:project_unique>/error/<int:error_id>', methods=['POST'])
-def update_error(project_unique, error_id):
-  project = Project.query.get(project_unique)
-  error = project.errors.filter_by(id=error_id).first()
+@app.route('/project/<string:project_unique>/defect/<int:defect_id>', methods=['POST'])
+def update_defect(project_unique, defect_id):
+  project = Projects.query.get(project_unique)
+  defect = project.defects.filter_by(id=defect_id).first()
   if request.form.get('false_positive', None) == 'True':
-    error.false_positive = request.form['false_positive']
-    error.save()
+    defect.false_positive = request.form['false_positive']
+    defect.save()
   if request.form.get('correct_minor_issue', None) == 'True':
-    error.correct_minor_issue()
-    error.story.re_chunk()
-    error.story.re_analyze()
+    defect.correct_minor_issue()
+    defect.story.re_chunk()
+    defect.story.re_analyze()
   return redirect(url_for('project', project_unique=project.id))
 
 @app.route('/project/<string:project_unique>/stories/update_story', methods=['POST'])
 def update_story(project_unique):
-  project = Project.query.get(project_unique)
+  project = Projects.query.get(project_unique)
   story = project.stories.filter_by(id=request.form['id'])[0]
-  story.text = request.form['value']
+  story.title = request.form['value']
   story.save()
   story.re_chunk()
   story.re_analyze()
-  return story.text
+  return story.title
 
 @app.route('/project/<string:project_unique>/correct_minor_issues', methods=['POST'])
 def correct_minor_issues(project_unique):
-  project = Project.query.get(project_unique)
-  for error in project.errors.filter_by(severity='minor').all():
-    error.correct_minor_issue()
+  project = Projects.query.get(project_unique)
+  for defect in project.defects.filter_by(severity='minor').all():
+    defect.correct_minor_issue()
   project.analyze()
   return redirect(url_for('project', project_unique=project.id))
+
+@app.route('/project/<string:project_unique>/analyze', methods=['GET'])
+def analyze_project(project_unique):
+  project = Projects.query.get(project_unique)
+  project.analyze()
+  return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route('/project/<string:project_unique>/stories/<string:story_unique>/analyze', methods=['GET'])
+def analyze_story(project_unique, story_unique):
+  project = Projects.query.get(project_unique)
+  story = project.stories.filter_by(id=story_unique).first()
+  story.re_chunk()
+  story.analyze()
+  return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 
 # @app.route('/backend/api/v1.0/stories', methods=['POST'])
 # def create_story():
 #   if not request.json or not 'text' in request.json or not 'project' in request.json:
 #     abort(400)
-#   story = Story.create(request.json['text'], request.json['project'])
+#   story = stories.create(request.json['text'], request.json['project'])
 #   if story.id:
 #     return jsonify({'story': story.serialize()}), 201
 #   else:
@@ -103,7 +118,7 @@ def correct_minor_issues(project_unique):
 # def create_project():
 #   if not request.json or not 'name' in request.json:
 #     abort(400)
-#   project = Project.create(request.json['name'])
+#   project = Projects.create(request.json['name'])
 #   if project.id:
 #     return jsonify({'project': project.serialize()}), 201
 #   else:
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -3,7 +3,7 @@ import unittest
 
 from config import basedir
 from app import app, db
-from app.models import Story, Project, Error
+from app.models import Story, Project, Defect
 
 class TestCase(unittest.TestCase):
   def setUp(self):
@@ -37,57 +37,57 @@ class WellFormednessTests(TestCase):
     p = create_project()
     s = Story.create(text="As a User, I'm able to add a user story, so that I document a requirement", project_id=p.id)
     s.analyze()
-    assert len(s.errors.all()) == 0
+    assert len(s.defects.all()) == 0
 
   def test_ends_one_comma(self):
     p = create_project()
     story = Story.create(text="As a User, I want to add a user story so that I document a requirement", project_id=p.id)
     p.analyze()
-    error = story.errors.all()[0]
-    assert error.kind == 'well_formed'
-    assert error.subkind == 'ends_one_comma'
-    assert error.severity == 'medium'
-    assert error.highlight == "As a User, I want to add a user story<span class='highlight-text'>,</span> so that I document a requirement"
+    defect = story.defects.all()[0]
+    assert defect.kind == 'well_formed'
+    assert defect.subkind == 'ends_one_comma'
+    assert defect.severity == 'medium'
+    assert defect.highlight == "As a User, I want to add a user story<span class='highlight-text'>,</span> so that I document a requirement"
 
   def test_no_comma(self):
     p = create_project()
     story = Story.create(text="As a User I want to add a user story so that I document a requirement", project_id=p.id)
     p.analyze()
-    error = story.errors.all()[0]
-    assert error.kind == 'well_formed'
-    assert error.subkind == 'no_comma'
-    assert error.severity == 'medium'
-    assert error.highlight == "As a User<span class='highlight-text'>,</span> I want to add a user story"
+    defect = story.defects.all()[0]
+    assert defect.kind == 'well_formed'
+    assert defect.subkind == 'no_comma'
+    assert defect.severity == 'medium'
+    assert defect.highlight == "As a User<span class='highlight-text'>,</span> I want to add a user story"
 
   def test_no_means(self):
     p = create_project()
     story = Story.create(text="As a User, I add a user story, so that I document a requirement", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='no_means')[0]
-    assert error.kind == 'well_formed'
-    assert error.subkind == 'no_means'
-    assert error.severity == 'high'
-    assert error.highlight == "Add a means"
+    defect = story.defects.filter(Defect.subkind=='no_means')[0]
+    assert defect.kind == 'well_formed'
+    assert defect.subkind == 'no_means'
+    assert defect.severity == 'high'
+    assert defect.highlight == "Add a means"
 
   def test_no_role(self):
     p = create_project()
     story = Story.create(text="User wants to add a user story, so that I document a requirement", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='no_role')[0]
-    assert error.kind == 'well_formed'
-    assert error.subkind == 'no_role'
-    assert error.severity == 'high'
-    assert error.highlight == "Add a role"
+    defect = story.defects.filter(Defect.subkind=='no_role')[0]
+    assert defect.kind == 'well_formed'
+    assert defect.subkind == 'no_role'
+    assert defect.severity == 'high'
+    assert defect.highlight == "Add a role"
 
 class AtomicTests(TestCase):
   def test_not_atomic(self):
     p = create_project()
     story = Story.create(text="As a User, I want to add a user story, so that I document a requirement and sell the system", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='conjunctions')[0]
-    assert error.kind == 'atomic'
-    assert error.subkind == 'conjunctions'
-    assert error.severity == 'high'
+    defect = story.defects.filter(Defect.subkind=='conjunctions')[0]
+    assert defect.kind == 'atomic'
+    assert defect.subkind == 'conjunctions'
+    assert defect.severity == 'high'
 
 class UniqueTests(TestCase):
   def test_identical(self):
@@ -95,11 +95,11 @@ class UniqueTests(TestCase):
     story = Story.create(text="As a User, I want to add a user story, so that I document a requirement and sell the system", project_id=p.id)
     story2 = Story.create(text="As a User, I want to add a user story, so that I document a requirement and sell the system", project_id=p.id)
     p.analyze()
-    error1 = story.errors.filter(Error.subkind=='identical')[0]
-    error2 = story2.errors.filter(Error.subkind=='identical')[0]
-    assert error1.kind == error2.kind == 'unique'
-    assert error1.subkind == error2.subkind == 'identical'
-    assert error1.severity == error2.severity == 'high'
+    defect1 = story.defects.filter(Defect.subkind=='identical')[0]
+    defect2 = story2.defects.filter(Defect.subkind=='identical')[0]
+    assert defect1.kind == defect2.kind == 'unique'
+    assert defect1.subkind == defect2.subkind == 'identical'
+    assert defect1.severity == defect2.severity == 'high'
 
 
 class MinimalTests(TestCase):
@@ -107,20 +107,20 @@ class MinimalTests(TestCase):
     p = create_project()
     story = Story.create(text="As a User, I want to add a user story, so that I document a requirement and sell the system. This is extra info", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='punctuation')[0]
-    assert error.kind == 'minimal'
-    assert error.subkind == 'punctuation'
-    assert error.severity == 'high'
+    defect = story.defects.filter(Defect.subkind=='punctuation')[0]
+    assert defect.kind == 'minimal'
+    assert defect.subkind == 'punctuation'
+    assert defect.severity == 'high'
 
 
   def test_brackets(self):
     p = create_project()
     story = Story.create(text="As a User, I want to add a user story, so that I document a requirement and sell the system (for money)", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='brackets')[0]
-    assert error.kind == 'minimal'
-    assert error.subkind == 'brackets'
-    assert error.severity == 'medium'
+    defect = story.defects.filter(Defect.subkind=='brackets')[0]
+    assert defect.kind == 'minimal'
+    assert defect.subkind == 'brackets'
+    assert defect.severity == 'medium'
 
 class UniformTests(TestCase):
   def test_uniformity(self):
@@ -129,9 +129,9 @@ class UniformTests(TestCase):
     Story.create(text="As a User, I want to add a user story, so that I document another requirement", project_id=p.id)
     story = Story.create(text="As a User, I am able to add a user story, so that I register a requirement", project_id=p.id)
     p.analyze()
-    error = story.errors.filter(Error.subkind=='uniform')[0]
-    assert error.kind == 'uniform'
-    assert error.severity == 'medium'
+    defect = story.defects.filter(Defect.subkind=='uniform')[0]
+    assert defect.kind == 'uniform'
+    assert defect.severity == 'medium'
 
 
 def create_project():
